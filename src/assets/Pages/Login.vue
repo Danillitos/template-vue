28b8ae768a8854681dafc6efe8ad8032f03f812d
Danillo Matos Garcez <danillo.garcez@live.com>
2025-04-12 11:31:27 -0300

    Reapply "Banco de dados implementado, cadastramento e login"
    This reverts commit 848db236cafe8087b7eb7959dbbb47861b5a4bc5.

--- src/assets/Pages/Login.vue
+++ src/assets/Pages/Login.vue
@@ -5,17 +5,16 @@
   let senha;
   const resposta = ref('');
 
-  function saveLogin() {
-
-    console.log("aa")
-    axios.post('http://localhost:3000/receber', {
-      username: username,
-      senha: senha
+  function getLogin() {
+    axios.get('http://localhost:3000/consulta', {
+      params: {
+        username: username,
+        senha: senha
+      }
     })
     .then(response => {
-      resposta.value = response.data.mensagem;
-      console.log(username, senha);
-    })
+      resposta.value = response.data.messagem
+    })   
   }
 
 
@@ -32,7 +31,7 @@
         <label for="senha">Senha</label>
         <input v-model="senha" type="password" class="border border-slate-500 bg-white rounded h-8 px-2">
       </div>
-      <button class="flex justify-center items-center bg-green-800 px-4 h-8 mt-4 rounded w-full cursor-pointer shadow text-white hover:opacity-80 transition duration-150 will-change-auto" @click="saveLogin()">
+      <button class="flex justify-center items-center bg-green-800 px-4 h-8 mt-4 rounded w-full cursor-pointer shadow text-white hover:opacity-80 transition duration-150 will-change-auto" @click="getLogin()">
         Enviar
       </button>
     </div>
@@ -41,6 +40,9 @@
     </div>
     <div>
       <p class="text-green-800 hover:opacity-80 cursor-pointer"><router-link to="/SignIn">Criar conta</router-link></p>
+    </div>
+    <div>
+      <p class="mt-4 text-red-500" v-if="resposta">{{ resposta }}</p>
     </div>
   </div>
 </template>
